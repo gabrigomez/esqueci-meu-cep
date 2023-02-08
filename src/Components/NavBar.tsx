a4c52269579d142e7067406b45b08fb146887ef3
Gabriel <gabrigomez15@gmail.com>
2023-02-08 15:05:44 -0300

    feat: elements in navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import { RiEmotionSadLine } from 'react-icons/ri';
+
 
 export const NavBar = () => {
   return (
-    <nav className='w-full h-20 bg-slate-200'>
-      Navbar 
+    <nav className='flex items-center justify-center w-full h-16 border border-b-slate-200'>
+      <div className='flex'>
+        <RiEmotionSadLine className='text-4xl mr-1'/>
+        <p className='font-shadows font-bold text-md'>
+          Esqueci meu CEP!
+        </p>
+      </div>
     </nav>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'shadows': ['Shadows Into Light', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
