94ac8ef8234e13a196ae01d236c5c50deabed2b4
Gabriel <gabrigomez15@gmail.com>
2023-02-13 10:07:31 -0300

    feat: add link to about page in navBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
 import { RiEmotionSadLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { BsInfoCircleFill } from 'react-icons/bs';
+
 
 export const NavBar = () => {
   return (
-    <nav className='flex items-center justify-center w-full h-16 border border-b-slate-200'>
-      <div className='flex'>
+    <nav className='flex justify-center items-center w-full h-16 border border-b-slate-200'>
+      <Link to='/about' className="absolute top-4 left-5 font-raleway">
+        <div className='flex items-center hover:opacity-80 duration-300'>
+          <BsInfoCircleFill className='mr-1' />
+          <p>
+            Sobre
+          </p>
+        </div>
+      </Link>
+      <div className='flex justify-self-center'>
         <RiEmotionSadLine className='text-5xl'/>
         <p className='font-julee font-semibold text-md'>
           ESQUECI MEU CEP!
         </p>
       </div>
-      <Link to='/sobre' className="">
-        Sobre
-      </Link>
     </nav>
   )
 }
