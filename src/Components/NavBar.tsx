e8f996bf4a36909364f29ff1bfb853b6a1c93f13
Gabriel <gabrigomez15@gmail.com>
2023-02-17 17:25:50 -0300

    style: add darkmode in navBar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -11,18 +11,18 @@ export const NavBar = () => {
   console.log(theme)
 
   return (
-    <nav className='flex justify-center items-center w-full h-16 border border-b-slate-200 dark:border-b-slate-800'>
-      <Link to='/about' className="absolute top-4 left-5 font-raleway">
+    <nav className='flex justify-center items-center w-full h-16 border-b border-b-slate-200 dark:border-b-slate-800'>
+      <Link to='/about' className="absolute top-4 left-5 dark:text-white font-raleway">
         <div className='flex items-center hover:opacity-80 duration-300'>
-          <BsInfoCircleFill className='mr-1' />
-          <p className=''>
+          <BsInfoCircleFill className='mr-1 dark:text-white' />
+          <p className='dark:text-white'>
             Sobre
           </p>
         </div>
       </Link>
       <Link to='/' className='flex justify-self-center'>
-        <RiEmotionSadLine className='text-5xl '/>
-        <p className='font-julee font-semibold text-md '>
+        <RiEmotionSadLine className='text-5xl dark:text-white '/>
+        <p className='font-julee font-semibold text-md dark:text-white '>
           ESQUECI MEU CEP!
         </p>
       </Link>
